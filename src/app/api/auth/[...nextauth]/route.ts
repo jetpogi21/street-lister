a01de15037bfc64f3f382d9a61c198f084555ff7
jetpogi21 <jet_pradas@yahoo.com>
2023-07-06 13:57:53 +0800

    modified:   .gitignore 	deleted:    src/app/(auth)/forgot-password/page.tsx 	deleted:    src/app/(auth)/login/page.tsx 	deleted:    src/app/(auth)/register/page.tsx 	deleted:    src/app/(auth)/reset-password/[token]/page.tsx 	deleted:    src/app/(auth)/verify/[token]/page.tsx 	deleted:    src/app/api/accounts/sync/route.ts 	deleted:    src/app/api/auth/[...nextauth]/route.ts 	deleted:    src/app/api/auth/forgot-password/route.ts 	deleted:    src/app/api/auth/reset-password/[token]/route.ts 	deleted:    src/app/api/auth/signup/route.ts 	deleted:    src/app/api/auth/verify-pw-token/[token]/route.ts 	deleted:    src/app/api/auth/verify/[token]/route.ts 	deleted:    src/app/api/password-tokens/sync/route.ts 	deleted:    src/app/api/posts/[id]/route.ts 	deleted:    src/app/api/posts/route.ts 	deleted:    src/app/api/posts/sync/route.ts 	deleted:    src/app/api/sessions/sync/route.ts 	deleted:    src/app/api/users/route.ts 	deleted:    src/app/api/users/sync/route.ts 	deleted:    src/app/api/verification-tokens/sync/route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { NextApiRequest, NextApiResponse } from "next";
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  return NextAuth(req, res, authOptions);
-};
-
-export { handler as GET, handler as POST };
